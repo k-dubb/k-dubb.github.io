143a34b4c69de4ebf9213419529f01a6c820e965
Kelli Waiss <kelliwaiss@hotmail.com>
2017-03-12 21:58:50 -0500

    removed tags

--- js/main.js
+++ js/main.js
@@ -28,13 +28,10 @@ $(document).ready(function() {
         }
     });
 
-<<<<<<< HEAD
-=======
     $('.carousel').carousel({
         interval: 8000
     });
 
->>>>>>> 8efecf3e834eef84bd3315b90506760fd849cbe8
     $('#mobile-nav .navigation a').click(function() {       
         $('.btn-navbar').click(); 
         $('.navbar-toggle').click();
@@ -76,8 +73,4 @@ $(document).ready(function() {
         }, 1000);
     });
   
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 8efecf3e834eef84bd3315b90506760fd849cbe8
