f24fd3813d540b5de34b661ea6687515775394dc
Kelli Waiss <kelliwaiss@hotmail.com>
2017-03-12 20:54:01 -0500

    Update main.js
    Add random skill highlighter and scroll to page

--- js/main.js
+++ js/main.js
@@ -1,35 +1,72 @@
 $(document).ready(function() {
 
-    $("#about").click(function (){
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= $('.nav-icons').offset().top) {
+            $('#hidden-nav').fadeIn(1000);  
+            $('.navbar').removeClass('hidden');  
+        }
+    });
+
+    setRandomClass();
+
+    setInterval(function() {
+        setRandomClass();
+    }, 1000);
+
+    function setRandomClass() {
+        var skillList = $(".skill-list li a");
+        randomSkill = Math.floor(Math.random() * 16); 
+        skillList.removeClass("selectList");
+        skillList.eq(randomSkill).addClass("selectList"); 
+    }
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() >= $('#parall').offset().top + $('#parall').outerHeight() - window.innerHeight) {
+            $('#past').delay(250).animate({'opacity': '1'}, 700);
+            $('#present').delay(725).animate({'opacity': '1'}, 700);
+            $('#future').delay(1200).animate({'opacity': '1'}, 700);    
+        }
+    });
+
+    $('#mobile-nav .navigation a').click(function() {       
+        $('.btn-navbar').click(); 
+        $('.navbar-toggle').click();
+    });
+
+    $("#about-me").click(function(){
         $('html, body').animate({
           scrollTop: $("#aboutMe").offset().top
-      }, 1000);
+        }, 1000);
     });
 
-    $("#projectSec").click(function (){
+    $("#skill-section").click(function(){
         $('html, body').animate({
-          scrollTop: $("#projectSection").offset().top
-      }, 1000);
+          scrollTop: $("#skillSection").offset().top
+        }, 1000);
     });
 
-    $("#journey").click(function (){
+    $("#about-me-hidden").click(function(){
         $('html, body').animate({
-          scrollTop: $("#project2").offset().top
-      }, 1000);
+          scrollTop: $("#aboutMe").offset().top
+        }, 1000);
     });
 
-    $("#contact").click(function (){
+    $("#skill-section-hidden").click(function(){
         $('html, body').animate({
-          scrollTop: $("#contactMe").offset().top
-      }, 1000);
+          scrollTop: $("#skillSection").offset().top
+        }, 1000);
     });
 
-    $('.darken').hover(
-    function(){
-       $(this).find('.message').fadeIn(500); 
-    },
-    function(){
-       $(this).find('.message').fadeOut(500); 
+    $("#about-me-mobile").click(function(){
+        $('html, body').animate({
+          scrollTop: $("#aboutMe").offset().top
+        }, 1000);
     });
 
+    $("#skill-section-mobile").click(function(){
+        $('html, body').animate({
+          scrollTop: $("#skillSection").offset().top
+        }, 1000);
+    });
+  
 });
